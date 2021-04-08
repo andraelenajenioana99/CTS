e08545eefc92c57c4eb2cc5784deccd9b4ad7f54
user <user@DESKTOP-SK50B26>
2021-04-08 16:55:36 +0300

    proxy

---
+++ seminar7_cts/src/ro/ase/cts/proxy/clase/Proxy.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.proxy.clase;
+
+public class Proxy implements CreditAbstract {
+	private CreditAbstract credit;
+	
+	public Proxy (CreditAbstract credit){
+		super();
+		this.credit = credit;
+	}
+	
+	public void OferaCredit(TipMoneda tip, float suma) { 
+		if (tip == TipMoneda.RON) {
+			credit.OferaCredit(tip, suma);
+		}
+		else {
+			System.out.println("Banca nu mai ofera credite in alte moende!");
+		}
+	}
+}
